99b3a228fa7530c3c826c97dd32a332dce9a8384
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2024-05-28 12:39:04 +0530

    tracking smart contract done

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -158,4 +158,42 @@ contract Tracking {
         emit ShipmentDelivered(_sender, _receiver, shipment.deliveryTime);
         emit ShipmentPaid(_sender, _receiver, amount);
     }
+
+    function getShipment(
+        address _sender,
+        uint256 _index
+    )
+        public
+        view
+        returns (
+            address,
+            address,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            shipmentStatus,
+            bool
+        )
+    {
+        Shipment memory shipment = shipments[_sender][_index];
+        return (
+            shipment.sender,
+            shipment.reciever,
+            shipment.pickupTime,
+            shipment.deliveryTime,
+            shipment.distance,
+            shipment.price,
+            shipment.status,
+            shipment.isPaid
+        );
+    }
+
+    function getShipmentsCount(address _sender) public view returns (uint256) {
+        return shipments[_sender].length;
+    }
+
+    function getAllTransaction() public view returns (typeShipment[] memory) {
+        return typeShipments;
+    }
 }
