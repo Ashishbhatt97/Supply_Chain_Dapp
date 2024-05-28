a6d0fe3ea58bd1f013a2eee2f6170c3291c20e37
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2024-05-28 14:41:39 +0530

    deploy script done

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,17 @@
-import hre from "hardhat";
+const hre = require("hardhat");
 
 const main = async () => {
-  const trackingContract = await hre.ethers.getContractFactory("Tracking");
-  const trackingInstance = await trackingContract.deploy();
+  try {
+    const trackingContract = await hre.ethers.getContractFactory("Tracking");
+    const trackingInstance = await trackingContract.deploy();
 
-  console.log(
-    `tracking Instance deployed at address ${await trackingInstance.getAddress()}`
-  );
-
-  main().catch((error) => {
+    console.log(
+      `tracking Instance deployed at address ${await trackingInstance.getAddress()}`
+    );
+  } catch (error) {
     console.error(error);
     process.exitCode = 1;
-  });
+  }
 };
+
+main();
