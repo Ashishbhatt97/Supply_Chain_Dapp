a5ccf3f0baa3df23b8c61642d162bae24590bfe1
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2024-07-05 13:51:33 +0530

    user profile section done

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,69 @@
-import { TrackingContext } from "@/Context/trackingContext";
-import React, { useContext } from "react";
+import { CircleUser, X } from "lucide-react";
+import * as React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({
+  openProfile,
+  currentUser,
+  setOpenProfile,
+  getShipmentCount,
+}: any) => {
+  const [count, setCount] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    const getShipment = () => {
+      const getShipmentsData = getShipmentCount();
+
+      return async () => {
+        const allData = await getShipmentsData();
+        setCount(allData);
+      };
+    };
+  }, []);
+  return openProfile ? (
+    <div className="fixed inset-0 z-10 overflow-y-auto">
+      <div
+        className="fixed inset-0 w-full h-full bg-black opacity-40 "
+        onClick={() => setOpenProfile(false)}
+      ></div>
+      <div className="flex items-center min-h-screen px-4 py-8 ">
+        <div className="relative w-full max-w-lg p-4 mx-auto bg-black rounded-md shadow-lg">
+          <div className="flex justify-end">
+            <button
+              className="p-2 text-gray-400 rounded-md hover:bg-gray-800"
+              onClick={() => setOpenProfile(false)}
+            >
+              <X className="text-white" />
+            </button>
+          </div>
+
+          <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+            <div className="flex flex-col mb-3 rounded-full shadow-lg ">
+              <CircleUser />
+              <h2 className="mb-1 text-xl font-medium text-gray-200 dark:text-white">
+                Welcome Trader
+              </h2>
+              <span className="text-sm text-gray-200">{currentUser}</span>
+              <div className="flex mt-4 space-x-3 md:mt-6">
+                <a
+                  href="#"
+                  className="items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg border-2"
+                >
+                  Balance: 34 ETH
+                </a>
+
+                <a
+                  href="#"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg border-2"
+                >
+                  Total Shipment : {count}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Profile;
